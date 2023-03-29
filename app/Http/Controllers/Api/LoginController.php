2cdaf8e8324b4bea88afb17e938201a57823485d
Ahmad Gozali <58220137+gozali97@users.noreply.github.com>
2023-03-30 06:40:34 +0700

    feat add api login and logout

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -9,28 +9,59 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     public function login(Request $request)
-{
-    $credentials = $request->only('email', 'password');
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
-    if (Auth::attempt($credentials)) {
-        $user = Auth::user();
-        $token = $user->createToken('MyApp')->accessToken;
-        $response = [
-            'success' => true,
-            'message' => 'Login success',
-            'data' => [
-                'user' => $user,
-                'token' => $token,
-            ],
-        ];
-        return response()->json($response);
-    } else {
-        $response = [
-            'success' => false,
-            'message' => 'Login failed',
-            'data' => [],
-        ];
-        return response()->json($response, 401);
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            if ($user->role->name === 'admin') {
+                return $this->success($user);
+            }
+
+            if ($user->role->name === 'guru') {
+                return $this->success($user);
+            }
+
+            if ($user->role->name === 'user') {
+                return $this->success($user);
+            }
+
+            // return response()->json([
+            //     'data' => [],
+            //     'status' => 'success',
+            //     'message' => 'Login successful',
+            // ]);
+        }
+
+        return $this->error("Email atau Password anda salah");
     }
-}
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        return $this->success('200');
+    }
+
+    public function success($data, $code = "200")
+    {
+        return response()->json([
+            'status' => 'Berhasil',
+            'code' => $code,
+            'data' => $data
+        ]);
+    }
+
+    public function error($message)
+    {
+        return response()->json([
+            'status' => 'Gagal',
+            'code' => 400,
+            'message' => $message
+        ], 400);
+    }
 }
